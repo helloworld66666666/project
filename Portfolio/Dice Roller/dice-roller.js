c10b0aa4c2bd35a53aa3cc81b23ad9d9188934db
Gerald <142395117+helloworld66666666@users.noreply.github.com>
2024-07-09 00:07:44 +0800

    Update dice-roller.js

--- Portfolio/Dice Roller/dice-roller.js
+++ Portfolio/Dice Roller/dice-roller.js
@@ -30,7 +30,7 @@ function rollDice(){
   for(let i = 0; i < numOfDice; i++){
     const value = Math.floor(Math.random() * 6) + 1;
     values.push(value);
-    images.push(`<img src="Dice Roller/dice-images/${value}.png" alt="Dice ${value}">`);
+    images.push(`<img src="dice_images/${value}.png" alt="Dice ${value}">`);
   }
   diceResult.textContent = `Dice: ${values.join(', ')}`;
   diceImages.innerHTML = images.join('');
